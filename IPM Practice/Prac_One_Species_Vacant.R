a3a3ac7ca9574623d9ad4cc0d6950f855b266f84
Ali Campbell <amc49@rice.edu>
2021-07-12 10:35:53 -0500

    Update Prac_One_Species_Vacant.R

--- IPM Practice/Prac_One_Species_Vacant.R
+++ IPM Practice/Prac_One_Species_Vacant.R
@@ -8,29 +8,31 @@ invlogit<-function(x){exp(x)/(1+exp(x))}
 
 ## ----------- Vital rate functions. Parameter indices are hard-coded and must correspond to rows of MCMC matrix ------------- ##
 #GROWTH FROM SIZE X TO Y
-gxy<-function(x,y,params,i){
+gxy<-function(x,y,params){
   xb=pmin(pmax(x,params[94]),params[95]) #Transforms all values below/above limits in min/max size (So the params are the minimums and maximums of size?)
   g_crem = dnorm(y,mean=params[1] + params[2]*xb,sd=params[3])
   g_vac = dnorm(y,mean=params[301] + params[302]*xb,sd = params[3])
-  ifelse(i == "crem", return(g_crem),return(g_vac))
+  return(list(g_crem=g_crem, g_vac=g_vac))
 }
 
 #SURVIVAL AT SIZE X.
-sx<-function(x,params,i){
+sx<-function(x,params){
   xb=pmin(pmax(x,params[94]),params[95])
   s_crem = invlogit(params[11] + params[12]*xb)
   s_vac = invlogit(params[311] + params[312]*xb)
-  ifelse(i == "crem", return(s_crem),return(s_vac))
+  return(list(s_crem=s_crem, s_vac=s_vac))
 }
 
 #SURVIVAL*GROWTH
-pxy<-function(x,y,params,i){
+pxy<-function(x,y,params){
   xb=pmin(pmax(x,params[94]),params[95])
-  return(sx(xb,params,i)*gxy(xb,y,params,i))
+  p_crem = sx(x,params)$s_crem * gxy(x,y,params)$g_crem
+  p_vac = sx(x,params)$s_vac * gxy(x,y,params)$g_vac
+  return(list(p_crem=p_crem, p_vac=p_vac))
 }
 
 #PRODUCTION OF 1-YO SEEDS IN THE SEED BANK FROM X-SIZED MOMS
-fx<-function(x,params,i){
+fx<-function(x,params,){
   xb=pmin(pmax(x,params[94]),params[95])        ## X dummy variable
   p.flow<-invlogit(params[31] + params[32]*xb)    ## Probability of Reproducing
   nflow<-exp(params[21] + params[22]*xb)          ## Number of FLowers produced
@@ -38,11 +40,11 @@ fx<-function(x,params,i){
   flow.surv_vac<-invlogit(params[341]) ## Proportion of Flowers survive to fruit
   seeds.per.fruit<-params[51]                     ## Number of Seeds per Fruit
   seed.survival<-invlogit(params[61])^2           ## Seed per Fruit Survival ---------I measured 6-month seed survival; annual survival is its square
-  ifelse(i == "crem",
-         (p.flow*nflow*flow.surv_crem*seeds.per.fruit*seed.survival),
-         (p.flow*nflow*flow.surv_vac*seeds.per.fruit*seed.survival)
-         )
+  f_crem = p.flow*nflow*flow.surv_crem*seeds.per.fruit*seed.survival
+  f_vac = p.flow*nflow*flow.surv_vac*seeds.per.fruit*seed.survival
+  return(list(f_crem=f_crem, f_vac=f_vac))
 }
+fx(x,params)
 
 #
 recruits<-function(y,params){
@@ -72,6 +74,7 @@ transition.x<-function(i,j,x){
 #GROWTH*SURVIVAL*ANT PROBABILITIES
 ptxy<-function(x,y,params,i,j){
   xb=pmin(pmax(x,params[94]),params[95])
+  vac_vac = sx(xb,params)$s_vac*gxy(xb,y,params)$g_vac*transition.x(i,j,xb)
   return(sx(xb,params,i)*gxy(xb,y,params,i)*transition.x(i,j,xb))
 }
 
@@ -94,8 +97,8 @@ bigmatrix<-function(params,lower,upper,matsize){
   Fmat<-matrix(0,(2*n+2),(2*n+2))
   
   # Banked seeds go in top row
-  fec1_v<-fx(y,params=params,"vac")
-  fec1_c<-fx(y,params=params,"crem")
+  fec1_v<-fx(y,params=params)$f_vac
+  fec1_c<-fx(y,params=params)$f_crem
   Fmat[1,3:(n+2)]<-fec1_c
   Fmat[1,(n+3):(2*n+2)]<-fec1_v
   
