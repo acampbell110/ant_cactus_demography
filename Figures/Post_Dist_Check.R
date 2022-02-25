074c961aaf422e82e2af255fa9d3c216ffd8e899
Ali Campbell <amc49@rice.edu>
2022-02-25 11:30:24 -0600

    Update Post_Dist_Check.R

--- Figures/Post_Dist_Check.R
+++ Figures/Post_Dist_Check.R
@@ -1,12 +1,12 @@
 setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Figures")
-source("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Data Analysis/Cholla_Analysis_Vital_Rates.R")
+source("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Cholla_Analysis_Vital_Rates.R")
 #### Y_rep checks
 
 ## Growth ################################################################################################
 #For overlay plots
-y <- y_grow
-grow_data <- read.csv("/Users/alicampbell/Dropbox/Ali and Tom -- cactus-ant mutualism project/Model Outputs/grow_outputs.csv", header = TRUE,stringsAsFactors=T)
-yrep_grow <- grow_yrep
+y <- growth_data$logsize_t1
+grow_out_all <- read.csv("/Users/alicampbell/Dropbox/Ali and Tom -- cactus-ant mutualism project/Model Outputs/grow_outputs_all.csv", header = TRUE,stringsAsFactors=T)
+yrep_grow <- grow_yrep_all#read.csv("/Users/alicampbell/Dropbox/Ali and Tom -- cactus-ant mutualism project/Model Outputs/grow_yrep_all.csv", header = TRUE,stringsAsFactors=T)-all
 samp100 <- sample(nrow(yrep_grow), 500)
 ## Overlay Plots
 png(file = "grow_post1.png")
@@ -17,26 +17,25 @@ bayesplot::ppc_dens_overlay_grouped(y, yrep_grow[samp100,], group = ant_grow)
 dev.off()
 ## Convergence Plots
 png("grow_conv2.png")
-bayesplot::mcmc_trace(As.mcmc.list(fit_grow_mix_ant, pars=c("beta0", "beta1")))
-title()
+bayesplot::mcmc_trace(As.mcmc.list(fit_grow_all, pars=c("beta0", "beta1")))
 dev.off()
 
 ## Survival ################################################################################################
 #overlay plot data
-y <- y_surv
+y <- survival_data$Survival_t1
 surv_data <- read.csv("/Users/alicampbell/Dropbox/Ali and Tom -- cactus-ant mutualism project/Model Outputs/surv_outputs.csv", header = TRUE,stringsAsFactors=T)
-yrep_surv <- surv_yrep
+yrep_surv <- surv_yrep_all
 samp100 <- sample(nrow(yrep_surv), 500)
 ## Overlay Plots
 png(file = "surv_post1.png")
 bayesplot::ppc_dens_overlay(y, yrep_surv[samp100,])
 dev.off()
 png(file = "surv_ant_post1.png")
-bayesplot::ppc_dens_overlay_grouped(y, yrep_surv[samp100,],group = ant_surv)
+bayesplot::ppc_dens_overlay_grouped(y, yrep_surv[samp100,],group = as.integer(as.factor(survival_data$ant_t)))
 dev.off()
 ## Convergence Plots
 png(file = "surv_conv1.png")
-bayesplot::mcmc_trace(As.mcmc.list(fit_surv_mix_ant, pars=c("beta0", "beta1")))
+bayesplot::mcmc_trace(As.mcmc.list(fit_surv_all, pars=c("beta0", "beta1")))
 dev.off()
 
 ## Reproductive State ################################################################################################
@@ -54,20 +53,6 @@ bayesplot::mcmc_trace(As.mcmc.list(fit_repro_mix_ant, pars=c("beta0", "beta1")))
 dev.off()
 
 
-## Flower Numbers ################################################################################################
-y <- y_flow
-yrep_flow <- flow_yrep
-samp100 <- sample(nrow(yrep_flow), 500)
-## Overlay Plots
-png(file = "flow_post1.png")
-bayesplot::ppc_dens_overlay(y, yrep_flow[samp100,])
-dev.off()
-## Convergence Plots
-png(file = "flow_conv1")
-bayesplot::mcmc_trace(As.mcmc.list(fit_flow_mix_ant, pars=c("beta0", "beta1")))
-dev.off()
-
-
 ## Flower Numbers Truncated ################################################################################################
 y_trunc <- y_flow
 # Create the y rep (needs to be done outside of STAN because of the 0 truncation)
@@ -75,23 +60,22 @@ y_flow_trunc_sim <- matrix(NA,1500,length(flower_data$TotFlowerbuds_t))
 for(i in 1:1500){
   print(i)
   for(j in 1:length(flower_data$TotFlowerbuds_t)){
-    y_flow_trunc_sim[i,j] <- sample(x=1:1000,size=1,replace=T,prob=dnbinom(1:1000, mu = exp(flow_pred[i,j]), size=phi_flow[i]) / (1 - dnbinom(0, mu = exp(flow_pred[i,j]), size=phi_flow[i])))
+    y_flow_trunc_sim[i,j] <- sample(x=1:1000,size=1,replace=T,prob=dnbinom(1:1000, mu = exp(flow_y_rep_all[i,j]), size=flow_phi_all[i]) / (1 - dnbinom(0, mu = exp(flow_y_rep_all[i,j]), size=flow_phi_all[i])))
   }
 }
-yrep_flow_trunc <- y_flow_trunc_sim
-samp100 <- sample(nrow(yrep_flow_trunc), 500)
+samp100 <- sample(nrow(y_flow_trunc_sim), 500)
 ## Overlay Plots
 png(file = "flow_post_trunc1.png")
-bayesplot::ppc_dens_overlay(y_trunc, yrep_flow_trunc[samp100,])
+bayesplot::ppc_dens_overlay(y_trunc, y_flow_trunc_sim[samp100,])
 dev.off()
 ## Convergence Plots
 png(file = "flow_conv_trunc1")
-bayesplot::mcmc_trace(As.mcmc.list(fit_flow_mix_ant_trunc, pars=c("beta0", "beta1")))
+bayesplot::mcmc_trace(As.mcmc.list(fit_flow_trunc_all, pars=c("beta0", "beta1")))
 dev.off()
 
 ## Viability of Flowers ################################################################################################
 y <- good_viab
-yrep_viab <- viab_yrep
+yrep_viab <- viab_yrep_all
 samp100 <- sample(nrow(yrep_viab), 500)
 ## Overlay Plots
 png(file = "viab_post1.png")
@@ -102,11 +86,9 @@ bayesplot::ppc_dens_overlay_grouped(y, yrep_viab[samp100,],group = ant_viab)
 dev.off()
 ## Convergence Plots
 png(file = "viab_conv1")
-bayesplot::mcmc_trace(As.mcmc.list(fit_viab_mix_ant, pars=c("beta0")))
+bayesplot::mcmc_trace(As.mcmc.list(fit_viab_all, pars=c("beta0")))
 dev.off()
 
-
-## Number of Fruit ################################################################################################
 
 ## Fruit Survival ################################################################################################
 y <- fruit.surv$Fr.on.grnd.not.chewed/fruit.surv$Fr.on.plant
