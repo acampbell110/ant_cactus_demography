ee26bb91ae3eb650d04efe167a513dfdd5ce8a57
Ali Campbell <amc49@rice.edu>
2022-05-19 10:03:10 -0500

    Update Analysis.R

--- IPM Code/Analysis.R
+++ IPM Code/Analysis.R
@@ -6,99 +6,75 @@
 
 setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/IPM Code")
 source("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/IPM Code/IPM.R")
+######## No Ants ####
+bigmatrix.1(params,lower,upper,matsize,1,"vacant")
+## Vacant = 0.9344461
 
-#### One ant results
-bigmatrix.1(4,4.5,lower,upper,matsize,1,"crem",(params))$IPMmat
-## Check that it works
-i = c("liom","vac","crem","other")
-j = c("vac","crem","crem","liom")
-x = c(-1,-5,4,3)
-y = c(-1,-4,4.5,3.01)
-big1 <- list()
-lambda1 <- list()
+
+######## One Ant Results ####
+#### Deterministic
+i <- c("crem","liom","other")
+i <- c("crem","liom","other")
+scenario = c("cremvac","liomvac","othervac")
+bmat <- vector()
 for(n in 1:length(i)){
-  big1[[n]] <- bigmatrix.1(x[n],y[n],lower,upper,matsize,1,i[n],params)
-  lambda1[[n]] <- Re(eigen(big1[[n]]$IPMmat)$values[1])
+  bmat[n] <- bigmatrix(params,lower,upper,matsize,2,i[n],j[n],scenario[n])
 }
-lambda1
+bmat
+## Crem & Vacant =  0.9369948
+## Liom & Vacant = 0.9348296
+## Other & Vacant = 0.9361773
+#### W Posterior
 
-## Crem
-crem_data <- subset(cactus, cactus$ant_t == "crem" & cactus$ant_t1 == "crem")
-i <- crem_data$ant_t
-j <- crem_data$ant_t1
-x <- crem_data$logsize_t
-y <- crem_data$logsize_t1
-big1 <- list()
-lambda1 <- list()
-lambda <- vector()
+######## Two Ant ####
+######## One Ant Results ####
+#### Deterministic
+i <- c("liom","other","vacant")
+j <- c("liom","other", "vacant")
+scenario = c("liomcremvac","liomvacother", "othercremvac" )
+bmat <- vector()
 for(n in 1:length(i)){
-  big1[[n]] <- bigmatrix.1(x[n],y[n],lower,upper,matsize,1,i[n],params)
-  lambda1[[n]] <- Re(eigen(big1[[n]]$IPMmat)$values[1])
+  bmat[n] <- bigmatrix(params,lower,upper,matsize,3,i[n],j[n],scenario[n])
 }
-lambda1
-## 0.9929766
+bmat
+## Liom & Crem & Vacant = 0.9369632
+## Liom & Other & Vacant = 0.9365571
+## Other & Crem & Vacant = 0.9382092
 
-## Other
-other_data <- subset(cactus, cactus$ant_t == "other" & cactus$ant_t1 == "other")
-i <- other_data$ant_t
-j <- other_data$ant_t1
-x <- other_data$logsize_t
-y <- other_data$logsize_t1
-big1 <- list()
-lambda1 <- list()
-lambda <- vector()
-for(n in 1:length(i)){
-  big1[[n]] <- bigmatrix.1(x[n],y[n],lower,upper,matsize,1,i[n],params)
-  lambda1[[n]] <- Re(eigen(big1[[n]]$IPMmat)$values[1])
-}
-lambda1
-## 0.9968325
+#### W Posterior
 
-## Liom
-liom_data <- subset(cactus, cactus$ant_t == "liom" & cactus$ant_t1 == "liom")
-i <- liom_data$ant_t
-j <- liom_data$ant_t1
-x <- liom_data$logsize_t
-y <- liom_data$logsize_t1
-big1 <- list()
-lambda1 <- list()
-lambda <- vector()
-for(n in 1:length(i)){
-  big1[[n]] <- bigmatrix.1(x[n],y[n],lower,upper,matsize,1,i[n],params)
-  lambda1[[n]] <- Re(eigen(big1[[n]]$IPMmat)$values[1])
-}
-lambda1
-## 0.9969405
+######## All Ant ####
+######## One Ant Results ####
+#### Deterministic
+bigmatrix(params,lower,upper,matsize,4,"crem","other","all")
+## All = 0.9388964
+#### W Posterior
 
-## vacant
-vac_data <- subset(cactus, cactus$ant_t == "vacant" & cactus$ant_t1 == "vacant")
-i <- vac_data$ant_t
-j <- vac_data$ant_t1
-x <- vac_data$logsize_t
-y <- vac_data$logsize_t1
-big1 <- list()
-lambda1 <- list()
-lambda <- vector()
-for(n in 1:length(i)){
-  big1[[n]] <- bigmatrix.1(x[n],y[n],lower,upper,matsize,1,i[n],params)
-  lambda1[[n]] <- Re(eigen(big1[[n]]$IPMmat)$values[1])
-}
-lambda1
-## 
+## Visualize the Deterministic Mean Lambdas
+## Vacant = 0.9344461
+## Crem & Vacant =  0.9369948
+## Liom & Vacant = 0.9348296
+## Other & Vacant = 0.9361773
+## Liom & Crem & Vacant = 0.9369632
+## Liom & Other & Vacant = 0.9365571
+## Other & Crem & Vacant = 0.9382092
+## All = 0.9388964
+lams <- as.data.frame(rep(NA,8))
+lams$means <- c(0.9344461,0.9369948,0.9348296,0.9361773,0.9369632,0.9365571,0.9382092,0.9388964)
+lams$scenario <- c("No Ants","Crem & Vacant","Liom & Vacant","Other & Vacant",
+                    "Liom & Crem & Vacant", "Liom & Other & Vacant", "Other & Crem & Vacant", "All Ants")
 
-#### Visualize the Growth Model
-min.size=min(c(cactus$logsize_t,cactus$logsize_t1),na.rm = T)
-max.size=max(c(cactus$logsize_t,cactus$logsize_t1),na.rm = T)
-# number of cells in the discretized kernel
-n=100 
-# boundary points (the edges of the cells defining the kernel)
-b=min.size+c(0:n)*(max.size-min.size)/n 
-# mesh points (midpoints of the cells)
-y=0.5*(b[1:n]+b[2:(n+1)])
-# width of the cells
-h=y[2]-y[1]
+setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Figures")
+png("lambda_means.png")
+plot(lams$means, col = c("Red","Blue","Green","Yellow","Orange","Brown","Black","Grey"), 
+     pch = 20, cex = 3, xlim = c(0,20),
+     xlab = "Ant Scenario", ylab = "Mean Lambda Value", main = "Lambdas by Ant Scenario")
+legend("topright",legend = lams$scenario, fill = c("Red","Blue","Green","Yellow","Orange","Brown","Black","Grey"))
+dev.off()
 
 
+
+## A visual of the growth kernel
 setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Figures")
 png("growth_heat.png")
 par(mfrow=c(2,2)) 
