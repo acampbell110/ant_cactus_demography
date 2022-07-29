b29f59abd513cec47b125646877f0a09282126d8
Ali Campbell <amc49@rice.edu>
2022-07-29 08:16:00 -0500

    Update Analysis.R

--- IPM Code/Analysis.R
+++ IPM Code/Analysis.R
@@ -6,8 +6,11 @@
 
 setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/IPM Code")
 source("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/IPM Code/IPM.R")
+
+lams <- as.data.frame(rep(NA,8))
+
 ######## No Ants ####
-bigmatrix.1(params,lower,upper,matsize,1,"vacant")
+lams$means[1] <- bigmatrix.1(params,lower,upper,matsize,0,"vacant")
 ## Vacant = 0.9344461
 
 
@@ -18,9 +21,9 @@ i <- c("crem","liom","other")
 scenario = c("cremvac","liomvac","othervac")
 bmat <- vector()
 for(n in 1:length(i)){
-  bmat[n] <- bigmatrix(params,lower,upper,matsize,2,i[n],j[n],scenario[n])
+  bmat[n] <- bigmatrix(params,lower,upper,matsize,1,i[n],j[n],scenario[n])
 }
-bmat
+lams$means[2:4] <- bmat
 ## Crem & Vacant =  0.9369948
 ## Liom & Vacant = 0.9348296
 ## Other & Vacant = 0.9361773
@@ -34,9 +37,9 @@ j <- c("liom","other", "vacant")
 scenario = c("liomcremvac","liomvacother", "othercremvac" )
 bmat <- vector()
 for(n in 1:length(i)){
-  bmat[n] <- bigmatrix(params,lower,upper,matsize,3,i[n],j[n],scenario[n])
+  bmat[n] <- bigmatrix(params,lower,upper,matsize,2,i[n],j[n],scenario[n])
 }
-bmat
+lams$means[5:7]<-bmat
 ## Liom & Crem & Vacant = 0.9369632
 ## Liom & Other & Vacant = 0.9365571
 ## Other & Crem & Vacant = 0.9382092
@@ -46,7 +49,7 @@ bmat
 ######## All Ant ####
 ######## One Ant Results ####
 #### Deterministic
-bigmatrix(params,lower,upper,matsize,4,"crem","other","all")
+lams$means[8]<-bigmatrix(params,lower,upper,matsize,3,"crem","other","all")
 ## All = 0.9388964
 #### W Posterior
 
@@ -59,37 +62,32 @@ bigmatrix(params,lower,upper,matsize,4,"crem","other","all")
 ## Liom & Other & Vacant = 0.9365571
 ## Other & Crem & Vacant = 0.9382092
 ## All = 0.9388964
-lams <- as.data.frame(rep(NA,8))
-lams$means <- c(0.9344461,0.9369948,0.9348296,0.9361773,0.9369632,0.9365571,0.9382092,0.9388964)
-lams$scenario <- c("No Ants","Crem & Vacant","Liom & Vacant","Other & Vacant",
-                    "Liom & Crem & Vacant", "Liom & Other & Vacant", "Other & Crem & Vacant", "All Ants")
-lams$scenario_abv <- c("V","C,V","L,V","O,V","L,C,V","L,O,V","O,C,V","All")
+lams$scenario <- c("No Ants","Crem.","Liom.","Oth.",
+                    "Liom. & Crem.", "Liom. & Oth.", "Oth. & Crem.", "All Ants")
+lams$scenario_abv <- c("None","C","L","O","L,C","L,O","O,C","All")
 setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Figures")
-png("lambda_means.png")
+png("lambda_means3.png")
 plot(lams$means, col = c("Red","Blue","Green","Yellow","Orange","Brown","Black","Grey"), 
-     pch = 20, cex = 4, ylim = c(0.933,0.94), xlim = c(0.5,length(lams$scenario)),
+     pch = 20, cex = 4, xlim = c(0.5,length(lams$scenario)), ylim = c(0.9740,0.9756),
      xaxt = "n",
      xlab = "Ant Scenario", ylab = "Mean Lambda Value", main = "Lambdas by Ant Scenario")
-# text(x = 1:length(lams$scenario)-0.2, y = lams$means +.0006, 
-#      labels = lams$scenario_abv,
-#      srt = 35)
-# legend("topright",legend = lams$scenario, fill = c("Red","Blue","Green","Yellow","Orange","Brown","Black","Grey"))
-# axis(1, at = c(1,2,3,4,5,6,7,8),labels = lams$scenario, srt = 45)
-# axis(side = 1, labels = FALSE)
-# text(seq(1, 8, by=1), par("usr")[3] - 0.2, labels = lams$scenario, srt = 45, pos = 1, xpd = TRUE)
-text(x = 1:length(lams$scenario),
-     ## Move labels to just below bottom of chart.
-     y = 0.932,#par("usr")[3]-0.1,
-     ## Use names from the data list.
-     labels = (lams$scenario_abv),
-     ## Change the clipping region.
-     xpd = NA,
-     ## Rotate the labels by 35 degrees.
-     srt = 35,
-     ## Adjust the labels to almost 100% right-justified.
-     adj = 0.965,
-     ## Increase label size.
-     cex = 1.2)
+ text(x = 1:length(lams$scenario)-0.2, y = lams$means +.00015,
+      labels = lams$scenario,
+      srt = 35)
+ legend("bottomright",legend = c("L = Liom.","C = Crem.","O = Other"))
+# text(x = 1:length(lams$scenario),
+#      ## Move labels to just below bottom of chart.
+#      y = 0.9738,#par("usr")[3]-0.1,
+#      ## Use names from the data list.
+#      labels = (lams$scenario),
+#      ## Change the clipping region.
+#      xpd = NA,
+#      ## Rotate the labels by 35 degrees.
+#      srt = 20,
+#      ## Adjust the labels to almost 100% right-justified.
+#      adj = 0.965,
+#      ## Increase label size.
+#      cex = 1)
 dev.off()
 
 
