5f2c030dcedf230426566b27f75c719ea958279f
Ali Campbell <amc49@rice.edu>
2022-08-04 12:15:08 -0500

    Update Params.R

--- IPM Code/Params.R
+++ IPM Code/Params.R
@@ -18,12 +18,6 @@ ceiling.extend=1.1*cholla_max
 lower<- cholla_min - floor.extend
 upper<- cholla_max + ceiling.extend
 
-n<-matsize
-L<-lower; U<-upper
-h<-(U-L)/n                   #Bin size
-b<-L+c(0:n)*h;               #Lower boundaries of bins 
-y<-0.5*(b[1:n]+b[2:(n+1)]);  #Bin midpoints
-
 
 ## -------- read in MCMC output ---------------------- ##
 #Ali
@@ -90,9 +84,8 @@ params$grow_beta14<-grow.params$beta1.4				## growth slope
 ##-----------------------Survival Parameters-----------------## 
 ## Check the names of the parameters
 #head(surv.params)
-params$surv_sig<-surv.params$sigma
-params$surv_sig_u<-surv.params$sigma_u
-params$surv_sig_w<-surv.params$sigma_w
+params$surv_sig_u<-surv.params$sigma_u        ## surv sigma u
+params$surv_sig_w<-surv.params$sigma_w        ## surv sigma w
 ####Ant 1 (vacant)
 params$surv_beta01<-surv.params$beta0.1     	  ## surv intercept
 params$surv_beta11<-surv.params$beta1.1				## surv slope
@@ -110,7 +103,6 @@ params$surv_beta14<-surv.params$beta1.4				## surv slope
 ## Check the names of the parameters
 #head(flow.params)
 params$flow_phi<-flow.params$phi     	      ## flow phi
-params$flow_sig<-flow.params$sigma				  ## flow sigma
 params$flow_sig_u<-flow.params$sigma_u        ## flow sigma u
 params$flow_sig_w<-flow.params$sigma_w        ## flow sigma w
 
@@ -122,21 +114,23 @@ params$flow_beta1<-flow.params$beta1          ## flow slopes
 #head(repro.params)
 params$repro_beta0<-repro.params$beta0      ## repro intercept
 params$repro_beta1<-repro.params$beta1      ## repro slope
+params$repro_sig_u<-repro.params$sigma_u    ## repro sigma u
+params$repro_sig_w<-repro.params$sigma_w    ## repro sigma w
 
 ##-----------------------Viability Parameters-----------------## 
 ## Check the names of the parameters
-#head(viab.params)
-params$viab_sig<-viab.params$sigma
-params$viab_sig_u<-viab.params$sigma_u
-params$viab_sig_w<-viab.params$sigma_w
+#head(viab.params) 
+params$viab_sig<-viab.params$sigma              ## viab sigma
+params$viab_sig_u<-viab.params$sigma_u          ## viab sigma u
+params$viab_sig_w<-viab.params$sigma_w          ## viab sigma w
 ####Ant 1 (vacant)
-params$viab_beta01<-viab.params$beta0.4     	  ## surv intercept
+params$viab_beta01<-viab.params$beta0.4     	  ## viab intercept
 ####Ant 2 (other)
-params$viab_beta02<-viab.params$beta0.3     	  ## surv intercept
+params$viab_beta02<-viab.params$beta0.3     	  ## viab intercept
 ####Ant 3 (crem)
-params$viab_beta03<-viab.params$beta0.1     	  ## surv intercept
+params$viab_beta03<-viab.params$beta0.1     	  ## viab intercept
 ####Ant 4 (liom)
-params$viab_beta04<-viab.params$beta0.2     	  ## surv intercept
+params$viab_beta04<-viab.params$beta0.2     	  ## viab intercept
 
 ##-----------------------Seeds Prod Parameters-----------------## 
 ## Check the names of the parameters
@@ -153,23 +147,23 @@ params$seed_beta03<-seed.params$beta0.3      ## seed intercept
 ##-----------------------Seeds Precensus Surv Parameters-----------------## 
 ## Check the names of the parameters
 #head(pre.seed.params)
-params$preseed_sig<-pre.seed.params$sigma
-params$preseed_sig_w<-pre.seed.params$sigma_w
-params$preseed_beta0<-pre.seed.params$beta0
+params$preseed_sig<-pre.seed.params$sigma         ## pre seed sigma
+params$preseed_sig_w<-pre.seed.params$sigma_w       ## pre seed sigma w
+params$preseed_beta0<-pre.seed.params$beta0       ## pre seed intercept
 
 ##-----------------------Germ1 Parameters-----------------## Ant 1 (crem)
 ## Check the names of the parameters
 #head(germ1.params)
-params$germ1_phi<-germ1.params$phi
-params$germ1_sig<-germ1.params$sigma
-params$germ1_beta0<-germ1.params$beta0
+params$germ1_phi<-germ1.params$phi            ## germ 1 phi
+params$germ1_sig<-germ1.params$sigma            ## germ 1 sigma
+params$germ1_beta0<-germ1.params$beta0        ## germ 1 intercept
 
 ##-----------------------Germ2 Parameters-----------------## Ant 1 (crem)
 ## Check the names of the parameters
 #head(germ2.params)
-params$germ2_phi<-germ2.params$phi
-params$germ2_sig<-germ2.params$sigma
-params$germ2_beta0<-germ2.params$beta0
+params$germ2_phi<-germ2.params$phi            ## germ 2 phi
+params$germ2_sig<-germ2.params$sigma        ## germ 2 sigma
+params$germ2_beta0<-germ2.params$beta0        ## germ 2 intercept
 
 ##------------------------- Recruitment-------------------##
 ## Check the names of the parameters
@@ -180,7 +174,7 @@ params$rec_sig<-rec.params$sigma         ## Rec error
 ##-------------------------Transition Parameters-------------------##
 
 ## Prev Vac
-params$multi_betavv <- multi.params$beta.1.1
+params$multi_betavv <- multi.params$beta.1.1      
 params$multi_betavo <- multi.params$beta.1.2
 params$multi_betavc <- multi.params$beta.1.3
 params$multi_betavl <- multi.params$beta.1.4
