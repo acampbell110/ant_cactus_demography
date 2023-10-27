ceef1dfb1af25ef9b2f599ba7e3d358d04eaf8b3
acampbell110 <73295157+acampbell110@users.noreply.github.com>
2023-10-27 11:11:46 -0500

    Update 01_cholla_ant_IPM_setup.R

--- 01_cholla_ant_IPM_setup.R
+++ 01_cholla_ant_IPM_setup.R
@@ -61,8 +61,8 @@ Q.kurtosis<-function(q.05,q.25,q.75,q.95){
 
 
 ## import the data -- Cacti (main) 
-cactus <- read.csv("Data Analysis/cholla_demography_20042021.csv", header = TRUE,stringsAsFactors=T)
-str(cactus) ##<- problem: antcount is a factor
+cactus <- read.csv("Data Analysis/cholla_demography_20042023.csv", header = TRUE,stringsAsFactors=T)
+# str(cactus) ##<- problem: antcount is a factor
 
 ## re-assign the seedling plots ("HT1B1" etc) to transects 1-3
 levels(cactus$Plot)<-c(levels(cactus$Plot),"T4")
@@ -84,17 +84,17 @@ cactus$Antcount_t1 <- as.numeric(as.character(cactus$Antcount_t1))
 #summary(as.factor(cactus$Antcount_t1))
 # assign ant counts of zero as vacant
 cactus$Antsp_t[cactus$Antcount_t==0] <- "vacant"
-summary(cactus$Antsp_t)
+#summary(cactus$Antsp_t)
 # here are the ordered levels of the current variable
 Antsp_t_levels <- levels(cactus$Antsp_t)
 # here is how I would like to collapse these into fewer bins -- most will be "other"
 ant_t_levels <- rep("other",times=length(Antsp_t_levels))
 # crem levels - elements
-ant_t_levels[c(5,8,9,10,38)] <- "crem"
+ant_t_levels[c(5,8,9,10,11,22,45)] <- "crem"
 # liom levels - elements
-ant_t_levels[c(17,21,22,23,24,25)] <- "liom"
+ant_t_levels[c(19,25,26,27,28,29)] <- "liom"
 # vacant - elements
-ant_t_levels[c(1,36,37,38)] <- "vacant"
+ant_t_levels[c(1,42,43,44)] <- "vacant"
 # create new variable merging levels as above
 cactus$ant_t <- factor(cactus$Antsp_t,levels=Antsp_t_levels,labels=ant_t_levels)
 # there are still some other problems with this data: first, there are no ant data from 2007:
@@ -118,11 +118,11 @@ Antsp_t1_levels <- levels(cactus$Antsp_t1)
 # here is how I would like to collapse these into fewer bins -- most will be "other"
 ant_t1_levels <- rep("other",times=length(Antsp_t1_levels))
 # crem levels
-ant_t1_levels[c(9,10,11,12,42)] <- "crem"
+ant_t1_levels[c(5,8,9,10,11,22,45)] <- "crem"
 # liom levels
-ant_t1_levels[c(23,24,25,26,27)] <- "liom"
+ant_t1_levels[c(27,28,29,30,31)] <- "liom"
 # vacant
-ant_t1_levels[c(1,40,41,39)] <- "vacant"
+ant_t1_levels[c(1,2,44,45)] <- "vacant"
 # create new variable merging levels as above
 cactus$ant_t1 <- factor(cactus$Antsp_t1,levels=Antsp_t1_levels,labels=ant_t1_levels)
 # check the spread of these ants
@@ -187,7 +187,7 @@ cactus$Survival_t1[cactus$Recruit == 1] <- NA
 cactus$Survival_t1[cactus$Newplant == 1] <- NA
 
 ## Export the data ---- Write a data file which has "cleaned" data with proper column headings
-head(cactus) # some of the columns are not names properly
+#head(cactus) # some of the columns are not names properly
 # rename all columns
 colnames(cactus) <- c("Plot" ,            "TagID"     ,       "Transplant"   ,    "Year_t" ,         
                       "Height_t"     ,    "Width_t"     ,     "Perp_t"      ,     "NS_t"   ,         
@@ -203,142 +203,9 @@ colnames(cactus) <- c("Plot" ,            "TagID"     ,       "Transplant"   ,  
 cactus <- cactus[ , c("Plot","TagID","Year_t","Goodbuds_t","TotFlowerbuds_t","ABFlowerbuds_t", "logsize_t","logsize_t1"                            ,"ant_t","ant_t1",
                       "Antcount_t","Year_t1","Recruit","Survival_t1","Goodbuds_t1","TotFlowerbuds_t1","ABFlowerbuds_t1"                            ,"Antcount_t1","flower1_YN","Newplant")]
 # Export cactus to a csv
-write.csv(cactus, "Data Analysis\cholla_demography_20042023_cleaned.csv")
+write.csv(cactus, "Data Analysis/cholla_demography_20042023_cleaned.csv")
 
 
-## Set up functions and subsets of data ----
-# 
-# ## Germination Data (No ant state)  ----
-# germ.dat_orig<-read.csv("Data Analysis/Germination.csv") 
-# germ.dat_orig$rate <- 0
-# for(i in 1:nrow(germ.dat_orig)){
-#   if(germ.dat_orig$Seedlings04[i] != 0){
-#     germ.dat_orig$rate[i] <- (germ.dat_orig$Seedlings04[i] - germ.dat_orig$Seedlings05[i])/germ.dat_orig$Seedlings04[i]
-#   }
-# }
-# germ.dat <- na.omit(germ.dat_orig)
-# germ.dat <- germ.dat[-c(42,39,40),]
-# #Check that you are happy with the subsetting
-# plot(germ.dat$rate)
-# points(germ.dat_orig$rate, col = "red")
-# # Most of the rows are included. We only lose 3, but it is still a small dataset. 
-# nrow(germ.dat)
-# nrow(germ.dat_orig)
-# 
-# ## Fruit Survival Data  (No ant state)  ----
-# seedling.dat_orig <- subset(cactus, cactus$Recruit == 1)
-# seedling.dat_orig <- seedling.dat_orig[,c("logsize_t1","Recruit","Year_t")]
-# nrow(seedling.dat_orig)
-# seedling.dat <- na.omit(seedling.dat_orig)
-# nrow(seedling.dat)
-# # losing 19 rows
-# # check that you are happy with the subsetting
-# plot(seedling.dat$logsize_t1, seedling.dat$Recruit, xlim = c(-5,15), ylim = c(0,1))
-# points(cactus$logsize_t1, cactus$Recruit, col = "red")
-# 
-# ##    Precensus Survival Data (No ant state)  ----
-# precensus.dat.orig<-read.csv("Data Analysis/PrecensusSurvival.csv") 
-# precensus.dat <- precensus.dat.orig[ , c("Transect","Seed","Log.size","survive0405")]
-# precensus.dat <- na.omit(precensus.dat)
-# # You lose no data here, but it is still a small dataset. 
-# nrow(precensus.dat)
-# nrow(precensus.dat.orig)
-# # check that you're happy with the subsetting
-# plot(precensus.dat$Log.size, jitter(precensus.dat$survive0405))
-# points(precensus.dat.orig$Log.size, jitter(precensus.dat.orig$survive0405), col = "red")
-# 
-# ## Growth Data (Includes Ant) ----   
-# # All Ant States
-# growth_data_orig <- cactus[ ,c("Plot","Year_t","Survival_t1","logsize_t","logsize_t1","ant_t")]
-# growth_data <- na.omit(growth_data_orig)
-# # Lose 2032 rows (due to plant death & recruit status)
-# nrow(growth_data_orig)
-# nrow(growth_data)
-# summary(growth_data$ant_t)
-# # check that you are happy with the subsetting
-# plot(growth_data$logsize_t, growth_data$logsize_t1)
-# points((cactus$logsize_t), (cactus$logsize_t1), col = "red")
-# 
-# ## Flowering Data  ----
-# flower_data_orig <- cactus[ , c("TotFlowerbuds_t", "logsize_t","Year_t","Plot")]
-# flower_data <- na.omit(flower_data_orig)
-# flower_data <- subset(flower_data, TotFlowerbuds_t > 0)
-# # Lose 6605 rows of data due to no flower data
-# nrow(flower_data_orig)
-# nrow(flower_data)
-# # check that you're happy with the subsetting
-# plot(flower_data$logsize_t, flower_data$TotFlowerbuds_t)
-# points(cactus$logsize_t, cactus$TotFlowerbuds_t, col = "red")
-# 
-# ##  Survival Data (Includes Ants)  ----
-# survival_data_orig <- cactus[ , c("Plot","Year_t","Survival_t1","ant_t","logsize_t")]
-# survival_data <- na.omit(survival_data_orig)
-# # Lose 1619 rows due to recruit status 
-# nrow(survival_data_orig)
-# nrow(survival_data)
-# # check that you're happy with the subsetting
-# plot(survival_data$logsize_t, (survival_data$Survival_t1))
-# points(cactus$logsize_t, cactus$Survival_t1, col = "red")
-# 
-# ##   Repro  Data ----
-# reproductive_data_orig <- cactus[ , c("flower1_YN","logsize_t","Year_t","Plot", "logsize_t1")]
-# reproductive_data <- na.omit(reproductive_data_orig)
-# # Lose 3332 rows of data because only including 
-# nrow(reproductive_data_orig)
-# nrow(reproductive_data)
-# # check that you're happy with the subsetting
-# plot(reproductive_data$logsize_t, reproductive_data$flower1_YN)
-# points(cactus$logsize_t, cactus$flower1_YN, col = "red")
-# 
-# ## Viability Data (Includes Ants)   ----
-# viability_data_orig <- cactus[ , c("TotFlowerbuds_t1","Goodbuds_t1","ABFlowerbuds_t1","ant_t", "logsize_t","Year_t","Plot")]
-# viability_data <- na.omit(viability_data_orig)
-# # Lose 3619 rows of data because of missing flower data
-# nrow(viability_data_orig)
-# viability_data <- subset(viability_data, TotFlowerbuds_t1 > 0)
-# nrow(viability_data)
-# # check if you're happy with the subsetting
-# plot(viability_data$logsize_t, viability_data$ABFlowerbuds_t1, xlim = c(-5,15), ylim = c(0,60))
-# plot(cactus$logsize_t, cactus$ABFlowerbuds_t1, col = "red") 
-# # This one is not ideal because there is missing ant data
-# 
-# ##        Import Seed Data and create Subsets ----
-# seed_uncleaned <- read.csv("Data Analysis/JO_fruit_data_final_dropplant0.csv", header = TRUE,stringsAsFactors=T)
-# # PEAA = Ant Access
-# # PAAA = Ant Access
-# # PEAE = Ant Exclusion
-# # PAAE = Ant Exclusion
-# seed <- subset(seed_uncleaned, treatment == "PAAA" | treatment == "PAAE")
-# nrow(seed)
-# # make the column for the ant state of the part of the plant producing seeds
-# for(i in 1:nrow(seed)){
-#   #If there is no ant access then vacant
-#   if(seed$ant.access[i] == "n" & is.na(seed$ant.access[i]) == FALSE){
-#     seed$ant_state[i] <- "Vacant"
-#   }
-#   #If there is ant access but it is still vacant then vacant
-#   if(seed$ant.access[i] == "y" & is.na(seed$ant.access[i]) == FALSE & seed$vacant[i] == "y"){
-#     seed$ant_state[i] <- "Vacant"
-#   }
-#   #if there is ant access and it is not vacant and the ant is crem then crem
-#   if(seed$ant.access[i] == "y" & is.na(seed$ant.access[i]) == FALSE & seed$vacant[i] == "n" & seed$species[i] == "c"){
-#     seed$ant_state[i] <- "Crem"
-#   }
-#   #if there is ant access and it is not vacant and the ant is liom then liom
-#   if(seed$ant.access[i] == "y" & is.na(seed$ant.access[i]) == FALSE & seed$vacant[i] == "n" & seed$species[i] == "l"){
-#     seed$ant_state[i] <- "Liom"
-#   }
-# }
-# seed_data <- seed
-# seed_data <- na.omit(seed_data)
-# seed_data$ant <- as.integer(as.factor(seed_data$ant_state))
-# seed_data$plant_fac <- as.integer(as.factor(seed_data$plant))
-# seed_data <- subset(seed_data, seed_count > 0)
-# nrow(seed_uncleaned)
-# nrow(seed_data)
-# # check if you're happy with the subsetting
-# #plot(seed$fruit_number)
-# #points(seed_data$fruit_number, col = "red")
 # 
 # ## Further analyses ----
 # # How many plants do we survey annually (on average)
