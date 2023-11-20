31e2505a61a8092ce2888bfcb405c1101a47c213
acampbell110 <73295157+acampbell110@users.noreply.github.com>
2023-11-20 11:46:48 -0600

    Update 05_cholla_ant_IPM_figures_tables.R

--- 05_cholla_ant_IPM_figures_tables.R
+++ 05_cholla_ant_IPM_figures_tables.R
@@ -1314,11 +1314,11 @@ setwd("/Users/alicampbell/Documents/GitHub/ant_cactus_demography/Figures")
 scenario_abv <- c("V","CV","LV","OV","LCV","LOV","OCV","LOCV")
 ## Plot the means of the deterministic and stochastic distributions together
 png("lambda_means.png")
-plot(c(1,3,5,7,9,11,13,15),colMeans(lams_dpost), pch = 20, cex = 5,col = cols,
+plot(c(1,3,5,7,9,11,13,15),(lams_dpost), pch = 20, cex = 5,col = cols,
      xlim = c(0,16), ylim = c(0.98,1.01),
      xaxt = "n",cex.lab = 2,
      xlab = "Ant Scenario", ylab = "Mean Lambda Value", main = "Full Partner Diversity Leads to \n Highest Fitness")
-text(x = c(1,3,5,7,9,11,13,15)-0.2, y = colMeans(lams_dpost)+0.002,cex = 2, labels = scenario_abv,srt = 35)
+text(x = c(1,3,5,7,9,11,13,15)-0.2, y = (lams_dpost)+0.002,cex = 2, labels = scenario_abv,srt = 35)
 legend("topleft",legend = c("L = Liom.","C = Crem.","O = Other"),cex = 1.5)
 points(c(1.5,3.5,5.5,7.5,9.5,11.5,13.5,15.5),colMeans(lams_stoch),
        col = cols, cex = 5)
